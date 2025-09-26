980e3357817614554b73582162386bfd1692297e
nimantha1999 <nimantha.bt@gmail.com>
2025-09-26 12:36:20 +0530

    implemented user registration

--- src/main/java/edu/icet/controller/UserController.java
+++ src/main/java/edu/icet/controller/UserController.java
@@ -1,4 +1,21 @@
 package edu.icet.controller;
 
+import edu.icet.dto.User;
+import edu.icet.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        userService.registerUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+    }
 }
--- src/main/java/edu/icet/dto/User.java
+++ src/main/java/edu/icet/dto/User.java
@@ -1,4 +1,27 @@
 package edu.icet.dto;
 
+import edu.icet.enums.RegistrationMethod;
+import edu.icet.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Integer userId;
+    private String firstName;
+    private String lastName;
+    private String primaryEmail;
+    private String passwordHash;
+    private String phone;
+    private Role role;
+    private RegistrationMethod registrationMethod;
+    private String oauthProvider;
+    private String oauthId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
