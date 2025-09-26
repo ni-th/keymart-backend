bed2d5a97a7926e6714bb742a08baf1b14362e54
nimantha1999 <nimantha.bt@gmail.com>
2025-09-26 13:00:56 +0530

    implemented product crud

--- src/main/java/edu/icet/repository/ProductRepository.java
+++ src/main/java/edu/icet/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package edu.icet.repository;
 
-public interface ProductRepository {
+import edu.icet.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
 }
--- src/main/java/edu/icet/service/impl/ProductServiceImpl.java
+++ src/main/java/edu/icet/service/impl/ProductServiceImpl.java
@@ -1,4 +1,40 @@
 package edu.icet.service.impl;
 
-public class ProductServiceImpl {
+import edu.icet.dto.Product;
+import edu.icet.entity.ProductEntity;
+import edu.icet.repository.ProductRepository;
+import edu.icet.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final ModelMapper mapper;
+
+    public Product createProduct(Product product) {
+        ProductEntity product1 = mapper.map(product, ProductEntity.class);
+        ProductEntity saved = productRepository.save(product1);
+        return mapper.map(saved, Product.class);
+    }
+
+    public Optional<Product> getProductById(Integer productId) {
+        return productRepository.findById(productId)
+                .map(product -> mapper.map(product, Product.class));
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> mapper.map(product, Product.class))
+                .collect(Collectors.toList());
+    }
+
+    public Product updateProduct(Product dto) {
+            return mapper.map(productRepository.save(mapper.map(dto, ProductEntity.class)), Product.class);
+    }
 }
