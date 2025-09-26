3607b8961d2e0d7936add77bc1b8b4def2757239
nimantha1999 <nimantha.bt@gmail.com>
2025-09-26 15:28:06 +0530

    implemented user service

--- src/main/java/edu/icet/controller/UserController.java
+++ src/main/java/edu/icet/controller/UserController.java
@@ -5,17 +5,26 @@ import edu.icet.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody User user) {
-        userService.registerUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+        if (userService.registerUser(user) != null){
+            return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+        }else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not registered successfully");
+        }
+
     }
 }
--- src/main/java/edu/icet/service/impl/UserServiceImpl.java
+++ src/main/java/edu/icet/service/impl/UserServiceImpl.java
@@ -1,27 +1,21 @@
 package edu.icet.service.impl;
 
-import edu.icet.entity.UserEntity;
 import edu.icet.dto.User;
+import edu.icet.entity.UserEntity;
 import edu.icet.repository.UserRepository;
+import edu.icet.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
+@Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
-
-    public UserEntity regiterUser(User user){
-        UserEntity save = userRepository.save(modelMapper.map(user, UserEntity.class));
-        return  save;
-    }
-    public UserEntity updateUser(UserEntity updatedUser) {
-        return userRepository.save(modelMapper.map(updatedUser, UserEntity.class));
-    }
-    public void deleteUser(Integer id) {
-        userRepository.deleteById(id);
+    
+    @Override
+    public UserEntity registerUser(User user) {
+        return userRepository.save(modelMapper.map(user, UserEntity.class));
     }
 }
